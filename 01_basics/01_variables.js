59a75e89a64c6dde249fcb26ec971730f6574724
AbhishekPrakhar <35653298+AbhishekPrakhar@users.noreply.github.com>
2023-08-12 03:55:15 +0000

    Datatypes discussion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountNum = 1125453
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "789456"
+accountCity = "Noida"
+
+// accountNum = 55646643
+
+console.log(accountNum);
+
+console.table([accountNum, accountEmail, accountPassword, accountCity])
