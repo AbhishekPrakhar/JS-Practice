d97c1564c8744e96adedf5002866742e6f722557
AbhishekPrakhar <35653298+AbhishekPrakhar@users.noreply.github.com>
2023-08-12 04:09:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber, typeof valueInNumber);
+
+// "33" => 33 - type number
+// "33abc" => NaN - type number
+// null => 0 - type number
+//undefined =
